8ae5c3f7e25a722067fb76a37131fcd0feeb0ded
Tran Vo Son Tung <sontungexpt@gmail.com>
2023-08-19 18:53:54 +0700

    fix: use default options for todo-comments

--- lua/plugins/configs/todo-comments.lua
+++ lua/plugins/configs/todo-comments.lua
@@ -4,30 +4,12 @@ local options = {
 	-- sign priority
 	sign_priority = 8,
 	-- keywords recognized as todo comments
-	keywords = {
-		FIX = {
-			icon = " ", -- icon used for the sign, and in search results
-			color = "error", -- can be a hex color, or a named color (see below)
-			alt = { "FIXME", "BUG", "FIXIT", "ISSUE" }, -- a set of other keywords that all map to this FIX keywords
-			-- signs = false, -- configure signs for some keywords individually
-		},
-		TODO = { icon = " ", color = "info" },
-		HACK = { icon = " ", color = "warning" },
-		WARN = { icon = " ", color = "warning", alt = { "WARNING", "XXX" } },
-		PERF = { icon = " ", alt = { "OPTIM", "PERFORMANCE", "OPTIMIZE" } },
-		NOTE = { icon = " ", color = "hint", alt = { "INFO" } },
-		TEST = { icon = "⏲ ", color = "test", alt = { "TESTING", "PASSED", "FAILED" } },
-	},
 	highlight = {
-		multiline = true, -- enable multine todo comments
-		multiline_pattern = "^.", -- lua pattern to match the next multiline from the start of the matched keyword
-		multiline_context = 10, -- extra lines that will be re-evaluated when changing a line
 		before = "", -- "fg" or "bg" or empty
 		keyword = "wide", -- "fg", "bg", "wide", "wide_bg", "wide_fg" or empty. (wide and wide_bg is the same as bg, but will also highlight surrounding characters, wide_fg acts accordingly but with fg)
 		after = "fg", -- "fg" or "bg" or empty
 		pattern = [[.*<(KEYWORDS)\s*:]], -- pattern or table of patterns, used for highlighting (vim regex)
 		comments_only = true, -- uses treesitter to match keywords in comments only
-		max_line_len = 400, -- ignore lines longer than this
 		exclude = {}, -- list of file types to exclude highlighting
 	},
 }
