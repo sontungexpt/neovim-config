0cd1c311beb0a2ced84fe5c9741c505164bd1b44
Tran Vo Son Tung <sontungexpt@gmail.com>
2023-09-19 07:39:52 +0700

    fix: use mason keymap default

--- lua/plugins/configs/mason.lua
+++ lua/plugins/configs/mason.lua
@@ -55,17 +55,6 @@ local options = {
 			package_installed = " ",
 			package_uninstalled = " ",
 		},
-		keymaps = {
-			toggle_server_expand = "<CR>",
-			install_server = "i",
-			update_server = "u",
-			check_server_version = "c",
-			update_all_servers = "U",
-			check_outdated_servers = "C",
-			uninstall_server = "X",
-			cancel_installation = "<C-c>",
-			apply_language_filter = "<C-f>",
-		},
 	},
 	max_concurrent_installers = 10,
 }
