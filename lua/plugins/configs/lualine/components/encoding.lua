b6e3072baefd3c471f00c9405d1a152895a72949
Tran Vo Son Tung <sontungexpt@gmail.com>
2023-10-03 15:38:49 +0700

    perl: improve encoding component in lualine

--- lua/plugins/configs/lualine/components/encoding.lua
+++ lua/plugins/configs/lualine/components/encoding.lua
@@ -1,31 +1,19 @@
 local colors = require("core.global-configs").ui.colors
 local no_seps = require("core.global-configs").ui.lualine.no_seps
 
+-- https://www.nerdfonts.com/cheat-sheet
+local icons = {
+	["utf-8"] = "",
+	["utf-16"] = "",
+	["utf-32"] = "",
+	["utf-8mb4"] = "",
+	["utf-16le"] = "",
+	["utf-16be"] = "",
+}
+
 local encoding = function()
-	-- get encoding and replace
-	-- utf8 with  (U+F75E)
-	-- utf16 with  (U+F75F)
-	-- utf32 with  (U+F780)
-	-- utf8mb4 with  (U+F781)
-	-- utf16le with  (U+F782)
-	-- utf16be with  (U+F783)
-	-- https://www.nerdfonts.com/cheat-sheet
 	local enc = vim.bo.fenc ~= "" and vim.bo.fenc or vim.o.enc
-	if enc == "utf-8" then
-		return ""
-	elseif enc == "utf-16" then
-		return ""
-	elseif enc == "utf-32" then
-		return ""
-	elseif enc == "utf-8mb4" then
-		return ""
-	elseif enc == "utf-16le" then
-		return ""
-	elseif enc == "utf-16be" then
-		return ""
-	else
-		return enc
-	end
+	return icons[enc] or enc
 end
 
 return {
